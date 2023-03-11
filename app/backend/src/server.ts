f0a4985e6aca99678d681b422ccd9165528ab2f1
Marcelo Vieira <marcelodefy15@gmail.com>
2023-03-11 13:26:19 -0300

    Env - Instalação e configuração de Eslint

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,7 +1,14 @@
 
-import App from "./app";
+import { Users } from '@prisma/client';
+import App from './app';
 import { prisma } from './services/prisma';
 
 const PORT = process.env.APP_PORT || 3001;
+
+const createUser = async (data: Users) => {
+  await prisma.users.create({ data });
+};
+
+createUser({ id: 'isudhdsiuhf', firstName: 'marcelo', lastName: 'Vieira', password: 'dfuihds', email: 'iusdhfiusdhfdush' });
 
 new App().start(PORT);
